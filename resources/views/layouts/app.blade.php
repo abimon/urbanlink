90f86e6b452fe397b2db86c40928ae8a5e607d51
abimon <eddyabby59@gmail.com>
2024-01-28 13:30:05 +0300

    color and logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -93,7 +93,7 @@
         <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="background-color: black;">
             <div class="container-fluid">
                 <a class="navbar-brand" href="/">
-                    <img src="{{asset('storage/images/logo2.png')}}" height="50">
+                    <img src="{{asset('storage/images/logo2.png')}}" height="80">
                 </a>
  
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
